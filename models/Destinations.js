4211eab4020ee9bb423425c6c6d9d23ef51d8355
unknown <jgdimeglio@gmail.com>
2015-05-11 00:34:06 +0200

    Formateando codigo

--- models/Destinations.js
+++ models/Destinations.js
@@ -3,9 +3,16 @@ var mongoose = require('mongoose');
 var DestinationSchema = new mongoose.Schema({
   title: String,
   location: {},
-  start: { type: Date},
-  end: { type: Date},
-  created_at: { type: Date ,default: new Date()},
+  start: {
+    type: Date
+  },
+  end: {
+    type: Date
+  },
+  created_at: {
+    type: Date,
+    default: new Date()
+  },
   travel: {
     type: mongoose.Schema.Types.ObjectId,
     ref: 'Travel'
--- public/javascripts/angularApp.js
+++ public/javascripts/angularApp.js
@@ -1,5 +1,6 @@
 var app = angular.module('vacaciones-permanentes', ['ui.router', 'angularMoment',
-    'google.places', 'uiGmapgoogle-maps', 'ui.calendar']);
+  'google.places', 'uiGmapgoogle-maps', 'ui.calendar'
+]);
 
 app.config([
   '$stateProvider',
@@ -142,8 +143,8 @@ app.factory('travels', ['$http', 'auth', function($http, auth) {
       });
     },
     removeDestination: function(travel, destination) {
-      return $http.delete('/travels/' + travel._id + '/' + destination._id + '/dest', 
-          headers());
+      return $http.delete('/travels/' + travel._id + '/' + destination._id + '/dest',
+        headers());
     },
     get: function(id) {
       return $http.get('/travels/' + id, headers()).then(function(res) {
@@ -213,15 +214,23 @@ app.controller('TravelCtrl', [
   function($scope, travels, travel, auth) {
     $scope.travel = travel;
     $scope.isLoggedIn = auth.isLoggedIn;
-    $scope.map = {center: {latitude: 0, longitude: 0 }, zoom: 2 };
-    $scope.options = {scrollwheel: false};
     $scope.destinationToRemove;
-	$scope.eventSources = [$scope.travel.destinations];
-$scope.uiConfig = {
-      calendar:{
+    $scope.map = {
+      center: {
+        latitude: 0,
+        longitude: 0
+      },
+      zoom: 2
+    };
+    $scope.options = {
+      scrollwheel: false
+    };
+    $scope.eventSources = [$scope.travel.destinations];
+    $scope.uiConfig = {
+      calendar: {
         height: 450,
         editable: false,
-        header:{
+        header: {
           left: '',
           center: 'title',
           right: 'prev,next'
@@ -229,67 +238,76 @@ $scope.uiConfig = {
       }
     }
 	
+    $scope.pol = {
+      id: 1,
+      path: [],
+      stroke: {
+        color: '#6060FB',
+        weight: 3
+      },
+      icons: [{
+        icon: {
+          path: google.maps.SymbolPath.BACKWARD_OPEN_ARROW
+        },
+        offset: '25px',
+        repeat: '50px'
+      }]
+    };
+
     $scope.addDestination = function() {
       if (!$scope.body.name || !$scope.body.start || !$scope.body.end) {
         return;
       }
       $scope.addMapPosition($scope.body);
-	  
-	  $scope.body.title=$scope.body.vicinity;
-	  $scope.body.location=$scope.body.geometry.location;
-	  
-      travels.addDestination(travel._id, $scope.body
-       ).success(function(destination) {
+
+      $scope.body.title = $scope.body.vicinity;
+      $scope.body.location = $scope.body.geometry.location;
+
+      travels.addDestination(travel._id, $scope.body).success(function(destination) {
         $scope.travel.destinations.push(destination);
-		$scope.body = null;
+        $scope.body = null;
       });
     };
-	
-	$scope.addAllMapPosition=function(destinations){
-		for(i=0;i<destinations.length;i++){
-			$scope.pol.path.unshift(new google.maps.LatLng(destinations[i].location.A,destinations[i].location.F));
-		}
+
+    $scope.addAllMapPosition = function(destinations) {
+      for (i = 0; i < destinations.length; i++) {
+        $scope.pol.path.unshift(new google.maps.LatLng(destinations[i].location.A, destinations[i].location.F));
+      }
     };
-	
-	$scope.addMapPosition=function(destination){
-		$scope.pol.path.unshift(destination.geometry.location);
+
+    $scope.addMapPosition = function(destination) {
+      $scope.pol.path.unshift(destination.geometry.location);
     };
-	
-    $scope.pol = { id: 1, path: [], stroke: { color: '#6060FB', weight: 3},
-        icons: [{icon: {path: google.maps.SymbolPath.BACKWARD_OPEN_ARROW},
-                    offset: '25px', repeat: '50px'
-                }]
-    };
-	
+
     $scope.destinationOptions = {
       types: ['(cities)']
     };
-	
+
     $scope.setDestinationToRemove = function(destination) {
       $scope.destinationToRemove = destination;
     }
-    
+
     $scope.removeDestination = function() {
       travels.removeDestination(travel, $scope.destinationToRemove).success(function(data) {
         var index = travel.destinations.indexOf($scope.destinationToRemove);
-		removePath(index);
+        removePath(index);
         travel.destinations.splice(index, 1);
       });
     };
-	
-	function removePath(index){
-		for(i=0;i < $scope.pol.path.length;i++){
-			if( equalsLocations($scope.pol.path[i],$scope.travel.destinations[index].location)){
-				$scope.pol.path.splice(i, 1);
-			}
-		}
-	}
-	
-	function equalsLocations(locationA,locationB){
-		return locationA.A == locationB.A && locationA.B == locationB.B; 
-	}
-	
-	$scope.addAllMapPosition(travel.destinations);
+
+    function removePath(index) {
+      for (i = 0; i < $scope.pol.path.length; i++) {
+        if (equalsLocations($scope.pol.path[i], $scope.travel.destinations[index].location)) {
+          $scope.pol.path.splice(i, 1);
+        }
+      }
+    }
+
+    function equalsLocations(locationA, locationB) {
+      return locationA.A == locationB.A && locationA.B == locationB.B;
+    }
+
+    $scope.addAllMapPosition(travel.destinations);
   }
 ]);
 
