281f8fba1740ebc2f76060ab5864b846b9abb256
unknown <jgdimeglio@gmail.com>
2015-06-09 16:24:10 +0200

    Test para el registro y login

--- spec/controllers/authSpec.js
+++ spec/controllers/authSpec.js
@@ -2,18 +2,88 @@
 
 describe("AuthCtrl", function() {
 
-    var scope, controller;
+  var scope, controller, httpBackend;
 
-    beforeEach(function(){
-        module('vacaciones-permanentes')
+  beforeEach(function() {
+    module('vacaciones-permanentes')
+  });
+
+  beforeEach(inject(function($controller, $rootScope, $injector) {
+    httpBackend = $injector.get('$httpBackend');
+    httpBackend.when('POST', '/register')
+      .respond({
+        toke: 'XXXX'
+      });
+
+    httpBackend.when('GET', '/view/home')
+      .respond({});
+    httpBackend.when('GET', '/travels')
+      .respond({});
+    httpBackend.when('GET', '/view/travels/all')
+      .respond({});
+
+    scope = $rootScope.$new();
+    controller = $controller('AuthCtrl', {
+      $scope: scope
     });
+  }));
 
-    beforeEach(inject(function($controller, $rootScope) {
-        scope = $rootScope.$new();
-        controller = $controller('AuthCtrl', { $scope: scope });
-    }));
+  it("Sin usuario al iniciar el sistema", function() {
+    expect(scope.user).toEqual({});
+  });
 
-    it("sin usuario", function() {
-        expect(scope.user).toEqual({});
-    });
+  it("Campos de registros obligatorios", function() {
+    var msg = 'Todos los campos son obligatorios';
+    httpBackend.expectPOST('/register', {})
+      .respond(401, msg);
+    scope.register();
+    httpBackend.flush();
+    expect(scope.error).toBe(msg);
+  });
+
+  it("Login usuario incorrecto", function() {
+    var msg = "El usuario es incorrecto";
+    httpBackend.expectPOST('/login', {
+        username: 'javier',
+        password: '1234'
+      })
+      .respond(401, msg);
+    scope.user.username = 'javier';
+    scope.user.password = '1234';
+    scope.logIn();
+    httpBackend.flush();
+    expect(scope.error).toBe(msg);
+  });
+
+  it("Usuario registrado correctamente", function() {
+    var msg;
+    httpBackend.expectPOST('/register', {
+        username: 'javier',
+        password: '1234'
+      })
+      .respond({
+        toke: 'XXXX'
+      });
+    scope.user.username = 'javier';
+    scope.user.password = '1234';
+    scope.register();
+    httpBackend.flush();
+    expect(scope.error).toBe(msg);
+  });
+
+  it("Login correcto", function() {
+    var msg;
+    httpBackend.expectPOST('/login', {
+        username: 'javier',
+        password: '1234'
+      })
+      .respond({
+        toke: 'XXXX'
+      });
+    scope.user.username = 'javier';
+    scope.user.password = '1234';
+    scope.logIn();
+    httpBackend.flush();
+    expect(scope.error).toBe(msg);
+  });
 });
