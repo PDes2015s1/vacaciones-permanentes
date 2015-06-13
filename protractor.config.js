2b5d490149c1df2eec4b24571609bfa12fe237a3
unknown <jgdimeglio@gmail.com>
2015-06-13 18:24:59 +0200

    Reiniciando la base de pruebas al terminar de correr los test con protractor

--- protractor.config.js
+++ protractor.config.js
@@ -1,5 +1,16 @@
-// conf.js
+var mongoose = require('mongoose');
+
 exports.config = {
-  seleniumAddress: 'http://localhost:4444/wd/hub',
+  // seleniumAddress: 'http://localhost:4444/wd/hub',
+  onComplete: function() {
+    mongoose.connect('mongodb://localhost/mydatabase', function() {
+      /* Drop the DB */
+      mongoose.connection.db.dropDatabase();
+    });
+
+  },
+  /*capabilities: {
+    'browserName': 'firefox'
+  },*/
   specs: ['tests/protractor/**/*Spec.js']
 }
